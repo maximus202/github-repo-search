ba234dc1a6843cc8e6ac7d0fb2f33dc471cb9871
Max R <maxx.ratto@gmail.com>
2019-11-16 12:29:16 -0700

    Final commit.

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 $(document).ready(function () {
 
-    const token = "";
+    const token = "06f0cc705677529f5725bbaf2c95405c1cfc5627";
 
     function getRepos(searchValue) {
-        const url = `https://api.github.com/users/:${searchValue}/repos`;
+        const url = `https://api.github.com/users/${searchValue}/repos`;
         const options = {
             headers: new Headers({
                 "Authorization": `Bearer ${token}`
             })
         };
         fetch(url, options)
-            .then(response => response.json())
-            .then(responseJson => console.log(responseJson));
-    };
-
-    /*function generateSearch(searchValue) {
-        console.log(`generateSearch ran with ${searchValue} paramater.`);
-        fetch(`https://api.github.com/users/:${searchValue}/repos`)
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -26,8 +19,8 @@ $(document).ready(function () {
             .then(responseJson => displayListOfRepos(responseJson))
             .catch(err => {
                 $('#js-error-message').text(`Something went wrong: ${err.message}`);
-            });
-    };*/
+            })
+    };
 
     function searchForRepos() {
         //User searches for a list of repos using a GitHub handle
@@ -41,9 +34,13 @@ $(document).ready(function () {
         });
     };
 
-    function displayListOfRepos() {
+    function displayListOfRepos(responseJson) {
         //User is displayed a list of repos by the repo name and a link to the repo URL
-        console.log('displayListOfRepos() ran')
+        console.log(`displayListOfRepos() with ${responseJson} ran`);
+        $('.js-results').empty();
+        for (let i = 0; i < responseJson.length; i++) {
+            $('.js-results').append(`<li>Repo name: <a href="${responseJson[i].html_url}">${responseJson[i].name}</a></li>`);
+        }
     };
 
     function clearResults() {
@@ -52,6 +49,5 @@ $(document).ready(function () {
     };
 
     searchForRepos();
-    displayListOfRepos();
     clearResults();
 });
